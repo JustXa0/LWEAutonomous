26e87c95e525681fb0b3ebc6d86f2a97431db887
BuildTools <unconfigured@null.spigotmc.org>
2023-03-25 15:32:21 -0500

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Adafruit_MotorShield.h>
 #include <SPI.h>
 
+
 Adafruit_MotorShield motorShield = Adafruit_MotorShield();
 Adafruit_DCMotor *leftMotor = motorShield.getMotor(1);
 Adafruit_DCMotor *rightMotor = motorShield.getMotor(2);
@@ -10,13 +11,10 @@ Adafruit_DCMotor *rightMotor = motorShield.getMotor(2);
 
 void setup() {
   // put your setup code here, to run once:
-
-
   
   motorShield.begin();
   leftMotor->setSpeed(150);
   rightMotor->setSpeed(150);
-  
 
   pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -24,15 +22,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   
-
-  rightMotor->fullOn();
-  leftMotor->fullOn();
   leftMotor->run(FORWARD);
   delay(1000);
-  leftMotor->fullOff();
+  leftMotor->run(RELEASE);
   rightMotor->run(FORWARD);
   delay(1000);
-  rightMotor->fullOff();
+  rightMotor->run(RELEASE);
 }
 
 void move(Adafruit_DCMotor motorName, Adafruit_DCMotor motorName2) {
