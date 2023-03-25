b6dcc61c5543ea6a43782aebe97807b757b03782
JustXa0 <76145042+JustXa0@users.noreply.github.com>
2023-03-25 16:00:12 -0500

    Dev (#1)
    * initalize pins
    
    * Updated source control and objects
    
    * Adjusted code to better use C++ principles
    
    * updated
    
    ---------
    
    Co-authored-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include <Arduino.h>
+#include <Adafruit_MotorShield.h>
+#include <SPI.h>
+
+
+Adafruit_MotorShield motorShield = Adafruit_MotorShield();
+Adafruit_DCMotor *leftMotor = motorShield.getMotor(1);
+Adafruit_DCMotor *rightMotor = motorShield.getMotor(2);
+
+
 
 void setup() {
   // put your setup code here, to run once:
+  
+  motorShield.begin();
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
+  
+  leftMotor->run(FORWARD);
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  leftMotor->run(RELEASE);
+  rightMotor->run(FORWARD);
   delay(1000);
+  rightMotor->run(RELEASE);
+}
+
+void move(Adafruit_DCMotor motorName, Adafruit_DCMotor motorName2) {
+  motorName.run(FORWARD);
+  motorName.run(FORWARD);
+
 }
