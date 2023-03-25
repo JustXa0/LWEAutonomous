8bbe71d86b33d8ee348cf38abf0c2a6c880164f2
BuildTools <unconfigured@null.spigotmc.org>
2023-03-25 13:11:34 -0500

    Updated source control and objects

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,41 @@
 #include <Arduino.h>
+#include <Adafruit_MotorShield.h>
+#include <SPI.h>
 
-/*
-   sensor array:
-   A0 thru A5 - sensors 2 thru 6
-   
+Adafruit_MotorShield motorShield = Adafruit_MotorShield();
+Adafruit_DCMotor leftMotor = Adafruit_DCMotor();
+Adafruit_DCMotor rightMotor = Adafruit_DCMotor();
 
-*/
+
 
 void setup() {
   // put your setup code here, to run once:
 
-  // initialzes all analog pins for input
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, INPUT);
 
-  pinMode(LED_BULITIN, OUTPUT);
+  
+  motorShield.begin();
+  leftMotor.setSpeed(150);
+  rightMotor.setSpeed(150);
+  
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
+
+  rightMotor.fullOn();
+  leftMotor.fullOn();
+  leftMotor.run(FORWARD);
+  delay(1000);
+  leftMotor.fullOff();
+  rightMotor.run(FORWARD);
+  delay(1000);
+  rightMotor.fullOff();
+}
+
+void move(Adafruit_DCMotor motorName, Adafruit_DCMotor motorName2) {
+  motorName.run(FORWARD);
+  motorName.run(FORWARD);
 }
