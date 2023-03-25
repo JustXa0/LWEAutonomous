f580982af1b673ac5f0b36d3fb57f50a5105629d
Alex Gorczowski <alexgorczow@gmail.com>
2023-03-25 18:14:41 -0500

    Updated to include the correct header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,28 @@
 #include <Arduino.h>
-#include <Adafruit_MotorShield.h>
+#include <AFMotor.h>
 #include <SPI.h>
 
 
-Adafruit_MotorShield motorShield = Adafruit_MotorShield();
-Adafruit_DCMotor *leftMotor = motorShield.getMotor(1);
-Adafruit_DCMotor *rightMotor = motorShield.getMotor(2);
+AFMotorController motorShield = AFMotorController();
+AF_DCMotor leftMotor(1, MOTOR12_2KHZ);
+AF_DCMotor rightMotor(2, MOTOR12_2KHZ);
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  
-  motorShield.begin();
-  leftMotor->setSpeed(150);
-  rightMotor->setSpeed(150);
-
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+  motorShield.enable();
+  leftMotor.setSpeed(255);
+  rightMotor.setSpeed(255);
+} 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  leftMotor.run(FORWARD);
+  rightMotor.run(FORWARD);
   
-  leftMotor->run(FORWARD);
-  delay(1000);
-  leftMotor->run(RELEASE);
-  rightMotor->run(FORWARD);
-  delay(1000);
-  rightMotor->run(RELEASE);
+
 }
 
-void move(Adafruit_DCMotor motorName, Adafruit_DCMotor motorName2) {
-  motorName.run(FORWARD);
-  motorName.run(FORWARD);
-
+void turnRight() {
+  
 }
